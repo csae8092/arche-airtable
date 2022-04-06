6840a51f2b0f86d4025a86f04db0683a9444c1ca
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-06 15:25:55 +0200

    some refactoring

--- play.py
+++ play.py
@@ -1,42 +1,20 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import os
-import pandas as pd
 from pyairtable import Base
-from rdflib import Graph, URIRef, Literal, XSD
-from rdflib.namespace import RDF
-from rdflib import Namespace
+from rdflib import Graph, URIRef
+from utils import make_predicate, make_object, yield_triple
 
-ACDH_NS = Namespace("https://example.org/")
 API_KEY = os.environ.get("AIRTABLE_API_KEY")
 BASE_ID = os.environ.get('BASE_ID', 'app6E6a4FoH5RjZsc')
-VALUES = {
-    'object_date_value': 'date',
-    'object_text_value': 'string',
-    'object_uri': 'uri',
-    'object_class_value': 'class'
-}
 base = Base(API_KEY, BASE_ID)
-predicate_table = base.get_table('ArcheObjects')
-rows = [x['fields'] for x in predicate_table.all()]
-df = pd.DataFrame(rows)
+main_table = base.get_table('ArcheObjects')
 g = Graph()
-for i, row in df.iterrows():
-    sub = URIRef(row['subject'])
-    obj = ()
-    for key, value in VALUES.items():
-        try:
-            obj = (row.dropna()[key], value)
-        except KeyError:
-            continue
-    if obj[1] == 'string':
-        g.add((sub, ACDH_NS[row['predicate_value'][0]], Literal(obj[0])))
-    elif obj[1] == 'date':
-        g.add((sub, ACDH_NS[row['predicate_value'][0]], Literal(obj[0], datatype=XSD.date)))
-    elif obj[1] == 'uri':
-        g.add((sub, ACDH_NS[row['predicate_value'][0]], URIRef(obj[0])))
-    elif obj[1] == 'class':
-        g.add((sub, RDF.type, ACDH_NS[row['object_class_value'][0]]))
+for x in yield_triple(main_table):
+    sub = URIRef(x['sub'])
+    pred = make_predicate(x['pred'])
+    obj = make_object(x)
+    print(sub, pred, obj)
+    g.add(
+        (sub, pred, obj)
+    )
 
 g.serialize('tmp_out.ttl')
---
+++ utils.py
@@ -0,0 +1,61 @@
+from rdflib import Namespace
+
+from rdflib import URIRef, Literal, XSD
+from rdflib.namespace import RDF
+
+
+ACDH_NS = Namespace("https://vocabs.acdh.oeaw.ac.at/acdh#")
+
+
+def get_lang_tag(value, sep='@@@'):
+    if sep in value:
+        lang = value.split(sep)[-1]
+        new_value = value.replace(f"{sep}{lang}", "")
+        return lang, new_value
+    else:
+        return '', value
+
+
+def yield_triple(table):
+    for x in table.iterate():
+        for y in x:
+            fields = y['fields']
+            try:
+                obj_key = [x for x in fields.keys() if '___' in x][0]
+            except IndexError:
+                continue
+            try:
+                triple = {
+                    "sub": fields['subject'],
+                    "pred": fields['predicate_value'][0],
+                    "obj_val": fields[obj_key],
+                    "obj_key": obj_key
+                }
+            except KeyError:
+                continue
+            yield triple
+
+
+def make_object(triple):
+    obj_type = triple['obj_key'].split('___')[1]
+    if obj_type == 'string':
+        lang_tag = get_lang_tag(triple['obj_val'])
+        if lang_tag[0]:
+            val = triple['obj_val'].split()
+            obj = Literal(lang_tag[1], lang=lang_tag[0])
+        else:
+             obj = Literal(triple['obj_val'])
+    elif obj_type == 'date':
+        obj = Literal(triple['obj_val'], datatype=XSD.date)
+    elif obj_type == 'uri':
+        obj = URIRef(triple['obj_val'])
+    elif obj_type == 'class':
+        obj = ACDH_NS[triple['obj_val'][0]]
+    return obj
+
+
+def make_predicate(pred_val):
+    if pred_val.startswith('rdf'):
+        return RDF.type
+    else:
+        return ACDH_NS[pred_val]
